cc6ba74e8ace48ead6e53b5812af6878a2438f0f
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-06-05 12:20:29 +0100

    Importing Your Own Files - challenge.

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const add = require("./utils");
+// Challenge: Define and use a function in a new file
+// 1. Create a new file called notes.js
+// 2. Create getNotes function that returns "Your notes..."
+// 3. Export getNotes function
+// 4. From app.js, load in and call the function printing the message to the console.
+const getNotes = require("./notes");
 
-const sum = add(2, 2);
+console.log(getNotes());
 
-console.log(sum);
+// const add = require("./utils");
+
+// const sum = add(2, 2);
+
+// console.log(sum);
 
 // const fs = require("fs");
 
 // fs.writeFileSync("notes.txt", "This file was created by Node.js.");
 
-// // Challnege: Aapend a message to notes.txt
+// // Challnege: Append a message to notes.txt
 
 // // 1. Use appendFileSync to append to the file
 
