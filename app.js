f7ae3747947d737a729fdf5d39cc1cc7f806bb3f
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-06-05 11:51:29 +0100

    Importing Node.js Core Modules - Challenge.

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const fs = require("fs");
 
-fs.writeFileSync("notes.txt", "This file was created by Node.js");
+fs.writeFileSync("notes.txt", "This file was created by Node.js.");
+
+// Challnege: Aapend a message to notes.txt
+
+// 1. Use appendFileSync to append to the file
+
+fs.appendFileSync("notes.txt", " This text was added to notes.txt by Node.js.");
+
+// 2. Run the script
