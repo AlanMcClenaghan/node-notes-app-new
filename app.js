53a49600ddd9546e2b2334f43de656e98039e458
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-06-06 12:32:10 +0100

    Argument Parsing with Yargs, Part 1

--- app.js
+++ app.js
@@ -1,74 +1,46 @@
-// Challenge: Use the chalk library in your project
-
-// 1. Install version 2.4.1 of chalk
-// 2. Load Chalk into app.js
 const chalk = require("chalk");
+const yargs = require("yargs");
+const getNotes = require("./notes.js");
 
-// 3. Use Chalk to print the string "Success!" to the console in green;
+// Customise yargs version
+yargs.version("1.1.0");
 
-const log = console.log;
+// Add, remove, read, list
 
-log(chalk.green("Success!"));
+// Create add Command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function() {
+    console.log("Adding a new note!");
+  }
+});
 
-// 4. Test your work.
+// Create remove Command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function() {
+    console.log("Removing a note!");
+  }
+});
 
-// BONUS: Use docs to mess around with other stlyes. Make text bold and inversed.
+// Create read Command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function() {
+    console.log("Reading a note!");
+  }
+});
 
-log(chalk.bold("Success in bold!"));
-log(chalk.inverse.green("Success in reverse!"));
+// Create list Command
+yargs.command({
+  command: "list",
+  describe: "List notes",
+  handler: function() {
+    console.log("Listing notes!");
+  }
+});
 
-// Examples of using chalk
-
-// Combine styled and normal strings
-log(chalk.blue("Hello") + " World" + chalk.red("!"));
-
-// Compose multiple styles using the chainable API
-log(chalk.blue.bgRed.bold("Hello world!"));
-
-// Pass in multiple arguments
-log(chalk.blue("Hello", "World!", "Foo", "bar", "biz", "baz"));
-
-// Nest styles
-log(chalk.red("Hello", chalk.underline.bgBlue("world") + "!"));
-
-// Nest styles of the same type even (color, underline, background)
-log(
-  chalk.green(
-    "I am a green line " +
-      chalk.blue.underline.bold("with a blue substring") +
-      " that becomes green again!"
-  )
-);
-
-// const validator = require("validator");
-
-// console.log(validator.isEmail("alandmcclenaghan@icloud.com"));
-// console.log(validator.isEmail("icloud.com"));
-// console.log(validator.isURL("icloud.com"));
-
-// Challenge: Define and use a function in a new file
-// 1. Create a new file called notes.js
-// 2. Create getNotes function that returns "Your notes..."
-// 3. Export getNotes function
-// 4. From app.js, load in and call the function printing the message to the console.
-// const getNotes = require("./notes");
-
-// console.log(getNotes());
-
-// const add = require("./utils");
-
-// const sum = add(2, 2);
-
-// console.log(sum);
-
-// const fs = require("fs");
-
-// fs.writeFileSync("notes.txt", "This file was created by Node.js.");
-
-// // Challnege: Append a message to notes.txt
-
-// // 1. Use appendFileSync to append to the file
-
-// fs.appendFileSync("notes.txt", " This text was added to notes.txt by Node.js.");
-
-// // 2. Run the script
+console.log(yargs.argv);
