cc6d9da9b1b444c47914db7f72a37c4197a28ee6
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-06-06 17:01:07 +0100

    Refactoring to use Arrow Functions

--- notes.js
+++ notes.js
@@ -1,15 +1,14 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-const getNotes = function() {
-  return "Your notes...";
-};
+const getNotes = () => "Your notes...";
 
-const addNote = function(title, body) {
+const addNote = (title, body) => {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter(function(note) {
-    return note.title === title;
-  });
+  const duplicateNotes = notes.filter(note => note.title === title);
+  // const duplicateNotes = notes.filter(note => {
+  //   return note.title === title;
+  // });
 
   if (duplicateNotes.length === 0) {
     notes.push({
@@ -23,11 +22,12 @@ const addNote = function(title, body) {
   }
 };
 
-const removeNote = function(title) {
+const removeNote = title => {
   const notes = loadNotes();
-  const filteredNotes = notes.filter(function(note) {
-    return note.title !== title;
-  });
+  const filteredNotes = notes.filter(note => note.title !== title);
+  // const filteredNotes = notes.filter(note => {
+  //   return note.title !== title;
+  // });
   savedNotes(filteredNotes);
   if (notes.length > filteredNotes.length) {
     console.log(chalk.inverse.green(`${title} removed!`));
@@ -36,12 +36,12 @@ const removeNote = function(title) {
   }
 };
 
-const savedNotes = function(notes) {
+const savedNotes = notes => {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync("notes.json", dataJSON);
 };
 
-const loadNotes = function() {
+const loadNotes = () => {
   try {
     const dataBuffer = fs.readFileSync("notes.json");
     const dataJSON = dataBuffer.toString();
